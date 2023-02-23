db0cea4b21c1488b5f518d6008184eabd4bd52ec
Jorge <jorgedguezm@gmail.com>
2023-02-23 18:24:19 +0100

    Catch possible Retrofit crashes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,28 +10,39 @@ import retrofit2.converter.gson.GsonConverterFactory
 import java.util.*
 
 fun main(args: Array<String>) = runBlocking {
+    // Credentials and baseUrl
     val user = args[0]
     val password = args[1]
-    val auth = Base64.getEncoder().encodeToString("$user:$password".toByteArray())
-    val url = "https://$user:$password@updates.dnsomatic.com/"
+    val baseUrl = "https://$user:$password@updates.dnsomatic.com/"
 
+    // Create ApiInterface object to make the requests.
     val okHttpClient = OkHttpClient.Builder().build()
     val gson: Gson = GsonBuilder().setLenient().create()
-    val retrofit = Builder().client(okHttpClient).baseUrl(url)
+    val retrofit = Builder().client(okHttpClient).baseUrl(baseUrl)
         .addConverterFactory(GsonConverterFactory.create(gson))
         .build()
     val apiInterface = retrofit.create(ApiInterface::class.java)
 
-    while (true) {
-        val response = apiInterface.post(auth)
-        println(response.code())
+    // Query parameter hostname and header authorization
+    val hostname = "all.dnsomatic.com"
+    val auth = Base64.getEncoder().encodeToString("$user:$password".toByteArray())
 
-        // Update IP every 30 minutes.
-        delay(1800000)
+    // Frequency
+    val minutes = 30
+
+    while (true) {
+        // Don't let losing your connection in a bad moment ruin your service.
+        runCatching { apiInterface.updateIpToDdns(hostname, auth) }
+            .onSuccess { println(it.code()) }
+            .onFailure { println(it.message) }
+            .also {
+                // Update IP every x minutes.
+                println("See you again in $minutes minutes")
+                delay(minutes * 60000L)
+            }
     }
 }
 
-suspend fun ApiInterface.post(auth: String) = withContext(Dispatchers.IO) {
-    val hostname = "all.dnsomatic.com"
+suspend fun ApiInterface.updateIpToDdns(hostname: String, auth: String) = withContext(Dispatchers.IO) {
     post(hostname, "Basic $auth")
 }
